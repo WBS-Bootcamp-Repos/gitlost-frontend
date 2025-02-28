1cbf67fb622bbbaa2d91c5c93c47587fa25e99c0
Nishanta Banik <nishanta.banik2016@gmail.com>
2025-02-28 14:51:33 +0100

    Adding the react config files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 via-green-500 to-red-500">
+    <div>
       <h1 className="text-7xl font-bold text-white">Travel Blog Frontend Interface</h1>
     </div>
   )
