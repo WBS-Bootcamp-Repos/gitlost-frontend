4a94d39547388c0a0bcd596a7f9e92bf15ae7d15
Nishanta Banik <nishanta.banik2016@gmail.com>
2025-02-28 15:00:01 +0100

    Adding files to dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-7xl font-bold text-white">Travel Blog Frontend Interface</h1>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 via-green-500 to-red-500">
+      <h1 className="text-5xl font-bold text-white animate-bounce shadow-lg p-6 rounded-lg">
+        Travel Blog Frontend Interface
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
